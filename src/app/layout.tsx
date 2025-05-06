84bdc6cc38795a3eefe86c5b315c08c706f0ffd9
Muneeb Hyder <muneeb@Muneebs-MacBook-Air.local>
2025-05-06 18:29:50 -0400

    acc nvm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,7 @@ export default function RootLayout({
         <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar /> {/* ✅ Navbar appears on every page */}
-        <main className="max-w-5xl mx-auto p-6">
-            <div className="bg-white rounded-2xl shadow-lg p-6">
-                {children}
-            </div>
-        </main>
+        <main>{children}</main>
         </body>
         </html>
     );
